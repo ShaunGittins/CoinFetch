e5fb05b193f27ed5dc7de0b98c05fe9efb73ea20
unknown <shaungg97@hotmail.com>
2021-10-13 17:56:26 +1100

    Remove BrowserRouter to work with GitHub pages (temp)

--- crypto-app/src/App.tsx
+++ crypto-app/src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import ListPage from './pages/ListPage';
+// import { BrowserRouter, Route } from 'react-router-dom';
 // import VolumeTrendPage from './pages/VolumeTrendPage';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <BrowserRouter>
-        <h1>Coin Fetch</h1>
-        <Route exact path="/" component={ListPage} />
-      </BrowserRouter>
+      <h1>Coin Fetch</h1>
+      <ListPage />
     </div>
   );
 }
