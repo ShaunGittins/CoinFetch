19c8fc6f9c7948c80f730114281d269ace9f21fa
unknown <shaungg97@hotmail.com>
2021-10-14 14:41:34 +1100

    Prettier format

--- crypto-app/src/components/CoinList/CoinList.test.jsx
+++ crypto-app/src/components/CoinList/CoinList.test.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CoinList from './CoinList';
 
-const mockCoins = [{
-  id: 'bitcoin',
-  symbol: 'btc',
-  name: 'Bitcoin',
-  image:
-    'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-  current_price: 55499,
-  market_cap: 1054317187416,
-  market_cap_rank: 1,
-  total_volume: 38358472924,
-  high_24h: 58066,
-  low_24h: 54705,
-  price_change_24h: -1614.447571454955,
-  price_change_percentage_24h: -2.82674,
-  market_cap_change_24h: -22080134743.47937,
-  market_cap_change_percentage_24h: -2.0513,
-}, 
-{
-    id: "ethereum",
-    symbol: "eth",
-    name: "Ethereum",
-    image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
+const mockCoins = [
+  {
+    id: 'bitcoin',
+    symbol: 'btc',
+    name: 'Bitcoin',
+    image:
+      'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 55499,
+    market_cap: 1054317187416,
+    market_cap_rank: 1,
+    total_volume: 38358472924,
+    high_24h: 58066,
+    low_24h: 54705,
+    price_change_24h: -1614.447571454955,
+    price_change_percentage_24h: -2.82674,
+    market_cap_change_24h: -22080134743.47937,
+    market_cap_change_percentage_24h: -2.0513,
+  },
+  {
+    id: 'ethereum',
+    symbol: 'eth',
+    name: 'Ethereum',
+    image:
+      'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
     current_price: 3476.26,
     market_cap: 413376320426,
     market_cap_rank: 2,
@@ -34,7 +36,7 @@ const mockCoins = [{
     price_change_percentage_24h: -0.5749,
     market_cap_change_24h: -171491803.04992676,
     market_cap_change_percentage_24h: -0.04147,
-  }
+  },
 ];
 
 it('renders coin names', () => {
--- crypto-app/src/components/ListPagination/ListPagination.test.jsx
+++ crypto-app/src/components/ListPagination/ListPagination.test.jsx
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react';
 import ListPagination from './ListPagination';
 
 it('renders essential components', () => {
-    render(<ListPagination/>);
-    const filterElement = screen.getByTestId('filter');
-    expect(filterElement).toBeInTheDocument();
-    expect(filterElement).toHaveTextContent('Filter:');
-    expect(screen.getByTestId('loading')).toBeInTheDocument();
-    expect(screen.getByTestId('controls')).toBeInTheDocument();
+  render(<ListPagination />);
+  const filterElement = screen.getByTestId('filter');
+  expect(filterElement).toBeInTheDocument();
+  expect(filterElement).toHaveTextContent('Filter:');
+  expect(screen.getByTestId('loading')).toBeInTheDocument();
+  expect(screen.getByTestId('controls')).toBeInTheDocument();
 });
 
 // TODO: test successful load of mock api.get data
