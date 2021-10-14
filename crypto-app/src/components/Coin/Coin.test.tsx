829eb2d933061dade091c8d44df393bad8fed6a4
unknown <shaungg97@hotmail.com>
2021-10-14 13:31:27 +1100

    Remove unused mockcoin data in tests

--- crypto-app/src/components/Coin/Coin.test.tsx
+++ crypto-app/src/components/Coin/Coin.test.tsx
@@ -11,7 +11,6 @@ const mockCoin = {
   current_price: 55499,
   market_cap: 1054317187416,
   market_cap_rank: 1,
-  fully_diluted_valuation: 1175050117827,
   total_volume: 38358472924,
   high_24h: 58066,
   low_24h: 54705,
@@ -19,17 +18,6 @@ const mockCoin = {
   price_change_percentage_24h: -2.82674,
   market_cap_change_24h: -22080134743.47937,
   market_cap_change_percentage_24h: -2.0513,
-  circulating_supply: 18842312,
-  total_supply: 21000000,
-  max_supply: 21000000,
-  ath: 64805,
-  ath_change_percentage: -13.65634,
-  ath_date: '2021-04-14T11:54:46.763Z',
-  atl: 67.81,
-  atl_change_percentage: 82418.20188,
-  atl_date: '2013-07-06T00:00:00.000Z',
-  roi: null,
-  last_updated: '2021-10-13T05:47:43.896Z',
 };
 
 it('renders Coin name', () => {
